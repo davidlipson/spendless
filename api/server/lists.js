36bfa7e3385602153c2151784cec42207d791624
David Lipson <davidlipson@Davids-MacBook-Pro.local>
2022-10-17 16:58:58 -0400

    changed list

--- api/server/lists.js
+++ api/server/lists.js
@@ -17,7 +17,7 @@ module.exports = {
             ],
         },
         cart: {
-            query: '#sns-base-price, .cart__subtotal-price, .cart__total-money, .sc-price, .a-price-whole, .price, .gl-body-l, .a-size-medium, .a-color-base, .sc-white-space-nowrap, .TableFoot-module--total__3H1ZL',
+            query: '#sns-base-price, .cart__subtotal-price, .cart__total-money, .sc-price, .a-price-whole, .price, .gl-body-l, .a-size-medium, .a-color-base, .sc-white-space-nowrap, .Heading-module--general__1ZZ-e, .TableFoot-module--td__2NdrQ, .TableFoot-module--total__3H1ZL, .Heading-module--small__3Jicy',
             regex: ['amazon.+/gp/cart', 'amazon.+/cart', '/cart'],
         },
     },
