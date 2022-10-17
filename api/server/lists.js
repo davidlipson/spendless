80be0977b1c44359071c1426b61b595163fca666
David Lipson <davidlipson@Davids-MacBook-Pro.local>
2022-10-17 19:43:51 -0400

    adidas

--- api/server/lists.js
+++ api/server/lists.js
@@ -17,7 +17,7 @@ module.exports = {
             ],
         },
         cart: {
-            query: '#sns-base-price, .cart__subtotal-price, .cart__total-money, .sc-price, .a-price-whole, .price, .gl-body-l, .a-size-medium, .a-color-base, .sc-white-space-nowrap, .Heading-module--general__1ZZ-e, .TableFoot-module--td__2NdrQ, .TableFoot-module--total__3H1ZL, .Heading-module--small__3Jicy',
+            query: '#sns-base-price, .cart__subtotal-price, .cart__total-money, .sc-price, .a-price-whole, .price, .gl-body-l, .a-size-medium, .a-color-base, .sc-white-space-nowrap, .Heading-module--general__1ZZ-e, .TableFoot-module--td__2NdrQ, .TableFoot-module--total__3H1ZL, .Heading-module--small__3Jicy, .gl-body-l',
             regex: ['amazon.+/gp/cart', 'amazon.+/cart', '/cart'],
         },
     },
