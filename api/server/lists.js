5ad261a530656cbdc347a4ea461dab81496e8876
David Lipson <davidlipson@Davids-MacBook-Pro.local>
2022-10-17 20:22:19 -0400

    whitelist

--- api/server/lists.js
+++ api/server/lists.js
@@ -5,7 +5,7 @@ module.exports = {
             regex: ['/gp/buy/thankyou'],
         },
         checkout: {
-            query: '.grand-total-price, .payment-due__price, .a-price-whole',
+            query: '.grand-total-price, .payment-due__price, .a-price-whole, .css-1tpw2mp, .eanm77i0, .eeazboz0, .css-1p2xd9r, .exmus2i5',
             description: '#productTitle, #title',
             regex: [
                 'amazon.+/gp/buy/',
@@ -17,7 +17,7 @@ module.exports = {
             ],
         },
         cart: {
-            query: '#sns-base-price, .cart__subtotal-price, .cart__total-money, .sc-price, .a-price-whole, .price, .gl-body-l, .a-size-medium, .a-color-base, .sc-white-space-nowrap, .Heading-module--general__1ZZ-e, .TableFoot-module--td__2NdrQ, .TableFoot-module--total__3H1ZL, .Heading-module--small__3Jicy, .gl-body-l, .css-1tpw2mp, .eanm77i0',
+            query: '#sns-base-price, .cart__subtotal-price, .cart__total-money, .sc-price, .a-price-whole, .price, .gl-body-l, .a-size-medium, .a-color-base, .sc-white-space-nowrap, .Heading-module--general__1ZZ-e, .TableFoot-module--td__2NdrQ, .TableFoot-module--total__3H1ZL, .Heading-module--small__3Jicy, .gl-body-l, .css-1tpw2mp, .eanm77i0, .eeazboz0, .css-1p2xd9r, .exmus2i5',
             regex: ['amazon.+/gp/cart', 'amazon.+/cart', '/cart', '/basket'],
         },
     },
