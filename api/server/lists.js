c346cd0534011f636a3a86a18455945694b4976e
David Lipson <davidlipson@Davids-MacBook-Pro.local>
2022-10-19 11:18:51 -0400

    quick push

--- api/server/lists.js
+++ api/server/lists.js
@@ -18,9 +18,16 @@ module.exports = {
         },
         cart: {
             query: '#sns-base-price, .cart__subtotal-price, .cart__total-money, .sc-price, .a-price-whole, .price, .gl-body-l, .a-size-medium, .a-color-base, .sc-white-space-nowrap, .Heading-module--general__1ZZ-e, .TableFoot-module--td__2NdrQ, .TableFoot-module--total__3H1ZL, .Heading-module--small__3Jicy, .gl-body-l, .css-1tpw2mp, .eanm77i0, .eeazboz0, .css-1p2xd9r, .exmus2i5, .total-row, .hdca-cart__summary-table-value-total, .h-text-lg, .h-text-bold, .cart-summary-line-item, .money-amount__main, .she-fr',
-            regex: ['/cart', '/basket', '/bag', '/orderitemdisplayview'],
+            regex: [
+                '/cart',
+                '/basket',
+                '/bag',
+                '/my-bag',
+                '/my-checkout',
+                '/orderitemdisplayview',
+            ],
         },
     },
     blacklist: ['amazon.+/dp/', 'amazon.+/gp/product/'],
-    totalRegex: '^(Estimated *)?(Order *)?(Sub *)?Total',
+    totalRegex: '^(Estimated *)?(Order *)?(Sub-? *)?Total',
 };
